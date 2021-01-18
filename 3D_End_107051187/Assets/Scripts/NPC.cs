0f1004323fad99804e4dbda2b39422e613fd9b96
Emrys0118 <71679826+Emrys0118@users.noreply.github.com>
2021-01-19 07:30:44 +0800

    結束

---
+++ 3D_End_107051187/Assets/Scripts/NPC.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+    public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirstDialog, Missioning, Finish
+    }
+
+    public NPCState state = NPCState.FirstDialog;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "阿明")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "阿明")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+    private void StopDialog()
+    {
+        dialog.SetActive(false);    
+        StopAllCoroutines();        
+    }
+    private IEnumerator Dialog()
+    {
+        dialog.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+        string dialogString = data.dialogB;
+
+        switch (state)
+        {
+            case NPCState.FirstDialog:
+                dialogString = data.dialogA;
+                break;
+            case NPCState.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case NPCState.Finish:
+                dialogString = data.dialogC;
+                break;
+        }
+
+        for (int i = 0; i < data.dialogA.Length; i++)
+        {
+            textContent.text += data.dialogA[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
